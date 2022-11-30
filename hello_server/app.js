bbf29b576cd6577d6f1aa32fb713e742fe0dbace
sam bourgault <samuelle.bourgault@gmail.com>
2022-11-30 09:33:24 -0800

    node app

--- hello_server/app.js
+++ hello_server/app.js
@@ -1,9 +1,21 @@
 var msg = 'Hello World';
 console.log(msg);
+const express = require('express');
+var app = express();
+var server = app.listen(3000);
+app.use(express.static("public"));
 
-const OSC = require('osc-js')
+var socket = require('socket.io');
+var io = socket(server);
+io.sockets.on('connection', newConnection);
 
-const config = { udpClient: { port: 9129 } }
-const osc = new OSC({ plugin: new OSC.BridgePlugin(config) })
+function newConnection(socket){
+    console.log("new connection: "+ socket.id)
+}
 
-osc.open() // start a WebSocket server on port 8080
+//const OSC = require('osc-js')
+
+//const config = { udpClient: { port: 9129 } }
+//const osc = new OSC({ plugin: new OSC.BridgePlugin(config) })
+
+//osc.open() // start a WebSocket server on port 8080
