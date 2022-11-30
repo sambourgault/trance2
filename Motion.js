a9d978d49c118358782f8dbdeabb10b36921d379
sam bourgault <samuelle.bourgault@gmail.com>
2022-11-29 21:54:24 -0800

    add motion

--- Motion.js
+++ Motion.js
@@ -7,6 +7,7 @@ window.addEventListener('devicemotion', function(e)
   z = parseFloat(e.acceleration.z).toFixed(3); 
 
   console.log("(", x, ", ", y, ", ", z, ") \n");
+  socket.emit('message', '/foo/bar'+x+' '+y+' '+z);
 });
 
 window.addEventListener('click', function()
@@ -18,64 +19,64 @@ function getPerm(){
   DeviceMotionEvent.requestPermission()
 }
 
-window.onload = function() {
-  var xebraState = new Xebra.State({
-    hostname: "192.168.0.145",
-    port: 8086,
-    supported_objects: Xebra.SUPPORTED_OBJECTS, 
-    auto_connect: false,
-    supported_objects: ["button"]
-  });
-  xebraState.connect();
+//  window.onload = function() {
+//   var xebraState = new Xebra.State({
+//     hostname: "192.168.0.145",
+//     port: 8086,
+//     supported_objects: Xebra.SUPPORTED_OBJECTS, 
+//     auto_connect: false,
+//     supported_objects: ["button"]
+//   });
+//   xebraState.connect();
 
-  function updateObj(object){
-    if (object.getParamValue("varname") === "slider") {
-      console.log("success");
-    }
-  }
+//   function updateObj(object){
+//     if (object.getParamValue("varname") === "slider") {
+//       console.log("success");
+//     }
+//   }
 
-  // Do something when a button gets added to the Max patcher
-  xebraState.on("object_added", function(object) {
-    if (object.type === "button") addHTMLButton(object);
-});
+//   // Do something when a button gets added to the Max patcher
+//   xebraState.on("object_added", function(object) {
+//     if (object.type === "button") addHTMLButton(object);
+// });
 
-// Do something when a button is removed
-xebraState.on("object_removed", function(object) {
-    if (object.type === "button") removeHTMLButton(object);
-});
+// // Do something when a button is removed
+// xebraState.on("object_removed", function(object) {
+//     if (object.type === "button") removeHTMLButton(object);
+// });
 
-  xebraState.on("object_added", updateObj);
+//   xebraState.on("object_added", updateObj);
 
-  xebraState.on("object_changed", updateObj);
+//   xebraState.on("object_changed", updateObj);
 
-  function resetPatcher() {
-    var patcher = xebraState.getPatchers(); // Assuming it's the first patcher
-    console.log(patcher);
-    var object = xebraState.getObjectsByScriptingName("slider");
-    console.log(object);
-    if (object) {
-        object.setParamValue("value", 75);
-    }
+//   function resetPatcher() {
+//     var patcher = xebraState.getPatchers(); // Assuming it's the first patcher
+//     console.log(patcher);
+//     var object = xebraState.getObjectsByScriptingName("slider");
+//     console.log(object);
+//     if (object) {
+//         object.setParamValue("value", 75);
+//     }
 
-  }
+//   }
 
-  var slider;
-  slider.setParamValue("value", 72);
-  resetPatcher();
-}
+//   var slider;
+//   slider.setParamValue("value", 72);
+//   resetPatcher();
+// }
 
-function addHTMLButton(object) {
-  var newButton = document.createElement("button");
-  newButton.id = "button-" + object.id;
-  newButton.onmousedown = function() {
-      object.setParamValue("value", 1);
-  };
-  newButton.onmouseup = function() {
-      object.setParamValue("value", 0);
-  };
-  newButton.appendChild(document.createTextNode("Button " + object.id));
-  document.getElementById("container").appendChild(newButton);
-}
+// function addHTMLButton(object) {
+//   var newButton = document.createElement("button");
+//   newButton.id = "button-" + object.id;
+//   newButton.onmousedown = function() {
+//       object.setParamValue("value", 1);
+//   };
+//   newButton.onmouseup = function() {
+//       object.setParamValue("value", 0);
+//   };
+//   newButton.appendChild(document.createTextNode("Button " + object.id));
+//   document.getElementById("container").appendChild(newButton);
+// }
 
 
 
