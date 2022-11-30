33caddc6e066bc19b280735616445b54804aaadc
sam bourgault <samuelle.bourgault@gmail.com>
2022-11-29 22:47:02 -0800

    rmv log

--- Motion.js
+++ Motion.js
@@ -6,7 +6,7 @@ window.addEventListener('devicemotion', function(e)
   y = parseFloat(e.acceleration.y).toFixed(3);
   z = parseFloat(e.acceleration.z).toFixed(3); 
 
-  console.log("(", x, ", ", y, ", ", z, ") \n");
+  //console.log("(", x, ", ", y, ", ", z, ") \n");
   socket.emit('message', '/foo/bar'+x+' '+y+' '+z);
 });
 
